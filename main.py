a60b2af266a79d634465cb6ac273df7f92b5c686
motefly <xuzhenhui@pku.edu.cn>
2018-12-25 08:42:38 +0000

    add pytorch version

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from utils import data_preprocess
+from model import DeepFM
+import torch
+
+result_dict = data_preprocess.read_criteo_data('./data/tiny_train_input.csv', './data/category_emb.csv')
+test_dict = data_preprocess.read_criteo_data('./data/tiny_test_input.csv', './data/category_emb.csv')
+
+deepfm = DeepFM.DeepFM(39,result_dict['feature_sizes'],verbose=True,use_cuda=True, weight_decay=0.0001,use_fm=True,use_ffm=False,use_deep=True).cuda()
+deepfm.fit(result_dict['index'], result_dict['value'], result_dict['label'],
+           test_dict['index'], test_dict['value'], test_dict['label'],ealry_stopping=True,refit=True)
